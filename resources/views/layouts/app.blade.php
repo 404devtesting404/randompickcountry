869a9a317db85172c88508e13c71a57a3bc0415f
404devtesting404 <404devtesting404@gmail.com>
2025-08-18 14:37:51 +0500

    Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
 </head>
 <style>
  header {
@@ -45,7 +45,7 @@ header nav a {
     <header>
         {{-- <a href="{{ route('country.random') }}" class="logo">🌎 RandomCountry</a> --}}
         <a href="{{ route('country.random') }}" class="logo">
-            <img src="{{ asset('images/logo.png') }}" alt="RandomCountry Logo" style="height:100px;">
+            <img src="{{ asset('public/images/logo.png') }}" alt="RandomCountry Logo" style="height:100px;">
         </a>
 
         {{-- <nav>
