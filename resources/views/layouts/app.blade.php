1d79ddd810b015ba45c97050833b4fa2e18c492f
404devtesting404 <404devtesting404@gmail.com>
2025-08-21 13:17:13 +0500

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,40 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @yield('meta')
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="preload"  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
 </head>
-<style>
- header {
-    display: flex;
-    align-items: center;
-    justify-content: center; /* logo center */
-    padding: 0px 0px;
-    box-shadow: 0 2px 5px rgba(0,0,0,0.1);
-    position: relative;
-}
-
-header .logo img {
-    max-height: 120px;
-    width: auto;
-}
-
-header nav {
-    position: absolute;
-    right: 30px;
-    top: 50%;
-    transform: translateY(-50%);
-}
-
-header nav a {
-    margin-left: 20px;
-    text-decoration: none;
-    font-weight: 500;
-    color: #333;
-}
-</style>
 <body>
     {{-- ✅ Success message --}}
     <header>
